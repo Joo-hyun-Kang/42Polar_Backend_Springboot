9e2e2dcb78a83e6d1f6d511a321ff2a3da306be5
Joo-hyun-Kang <autoba9687@gmail.com>
2025-01-01 16:52:28 +0900

    feat: フロントのメインページでカテゴリー見せる時に使っている Issues: #3

--- src/main/java/com/_polar/_polar_backend_spring/v1/categories/CategoriesController.java
+++ src/main/java/com/_polar/_polar_backend_spring/v1/categories/CategoriesController.java
@@ -1,20 +1,35 @@
 package com._polar._polar_backend_spring.v1.categories;
 
 import com._polar._polar_backend_spring.domain.entity.Categories;
+import com._polar._polar_backend_spring.v1.categories.dto.response.CategoriesDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("api/v1/categories")
 public class CategoriesController {
     private final CategoriesRepository categoriesRepository;
 
-    @GetMapping("categories")
-    public List<Categories> getCategories() {
-        return categoriesRepository.findAll();
+    /*
+     * フロントのメインページでカテゴリー見せる時に使っている
+     */
+    @GetMapping()
+    public List<CategoriesDto> getCategories() {
+        List<Categories> categories = categoriesRepository.findAll();
+        List<CategoriesDto> result = new ArrayList<>();
+        for (Categories category : categories) {
+            CategoriesDto categoriesDto = new CategoriesDto();
+            categoriesDto.setName(category.getName());
+            result.add(categoriesDto);
+        }
+
+        return result;
     }
 }
---
+++ src/main/java/com/_polar/_polar_backend_spring/v1/categories/dto/response/CategoriesDto.java
@@ -0,0 +1,8 @@
+package com._polar._polar_backend_spring.v1.categories.dto.response;
+
+import lombok.Data;
+
+@Data
+public class CategoriesDto {
+    private String name;
+}
